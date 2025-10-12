99773bea445b0139d28fce46cc7c683870b3704b
Yegor Tchitaev <yegor.tchitaev@gmail.com>
2025-10-13 00:27:50 +1100

    Add page config to streamlit

--- app/app.py
+++ app/app.py
@@ -8,6 +8,12 @@ if 'messages' not in st.session_state:
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
 
+st.set_page_config(
+    page_title="Chatbot",  
+    page_icon="📄", 
+    layout="wide"
+)
+
 st.title("PDF Documents Chatbot")
 
 # display chat history
